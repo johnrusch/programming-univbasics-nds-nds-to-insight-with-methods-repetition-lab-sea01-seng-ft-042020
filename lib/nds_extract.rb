71d0fb14b95674f82568f95982864deb4d8d3ef1
John Rusch <johnrusch7@gmail.com>
2020-03-23 19:21:15 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,7 +13,8 @@ def directors_totals(source)
     result[director[:name]] = gross_for_director(director)
     director_index += 1
   end
-  result
+  pp result
+  binding.pry
 end
 
 def gross_for_director(d)
@@ -52,6 +53,9 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  
+  row_index = 0 
+  
 end
 
 
